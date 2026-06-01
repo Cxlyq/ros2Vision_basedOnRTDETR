bce993d164e2f33f73f4ef265fba76e810acbbea
Xu Chen <cx3348269780@outlook.com>
2026-06-01 11:31:07 +0800

    feat(dt_matcher): 为该节点配置launch

---
+++ src/dt_matcher/launch/dt_matcher.launch.py
@@ -0,0 +1,25 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_name = 'dt_matcher'
+
+    # 获取参数文件路径
+    pkg_dir = get_package_share_directory(pkg_name)
+    config_path = os.path.join(pkg_dir, 'config', 'dt_matcher.yaml')
+
+    # 定义 dt_matcher 节点
+    dt_matcher_node = Node(
+        package=pkg_name,
+        executable='dt_matcher.py',
+        name='dt_matcher',
+        output='screen',
+        parameters=[config_path],
+    )
+
+    return LaunchDescription([
+        dt_matcher_node
+    ])
